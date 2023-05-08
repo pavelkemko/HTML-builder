c77786f320efbddcca313e0955e5890ef8a1ec6d
pavelkemko <pavelkemko@gmail.com>
2023-05-08 20:05:35 +0300

    feat: add solution for the first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require ('fs');
+const path = require ('path');
+const filePath = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(filePath, 'utf8');
+readableStream.on('data', function(chunk){ 
+  console.log(chunk);
+});
